370f933cd0433e782fe0ce284b4641ca86073f7c
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-08-10 16:44:45 -0300

    tchoin

--- SITE/TDSAlmoxarifado/TDSAlmoxarifado/TDSAlmoxarifado/Controllers/AreaController.cs
+++ SITE/TDSAlmoxarifado/TDSAlmoxarifado/TDSAlmoxarifado/Controllers/AreaController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TDSAlmoxarifado.Models;
 
 namespace TDSAlmoxarifado.Controllers
 {
     public class AreaController : Controller
     {
+        BDTDSAlmoxarifado bd = new BDTDSAlmoxarifado();
         // GET: Area
         public ActionResult Index()
         {
-            return View();
+            return View(bd.AREA.ToList());
         }
 
         [HttpGet]
@@ -20,14 +22,42 @@ namespace TDSAlmoxarifado.Controllers
             return View();
         }
 
-        [HttpGet]
-        public ActionResult Edit()
+        [HttpPost]
+        public ActionResult Create(string descricao)
         {
-            return View();
+            AREA novaArea = new AREA();
+            novaArea.AREDESCRICAO = descricao;
+            bd.AREA.Add(novaArea);
+            try
+            {
+                bd.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("ErroBD", "Home");
+            }
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
-        public ActionResult Delete()
+        public ActionResult Editar(int id, string descricao)
+        {
+            AREA updateArea = bd.AREA.ToList().Where(y => y.AREID == id).First();
+            updateArea.AREDESCRICAO = descricao;
+            bd.Entry(updateArea).State = System.Data.Entity.EntityState.Modified;
+            try
+            {
+                bd.SaveChanges();
+            }
+            catch
+            {
+                return RedirectToAction("ErroBD", "Home");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Excluir()
         {
             return View();
         }
--- SITE/TDSAlmoxarifado/TDSAlmoxarifado/TDSAlmoxarifado/Controllers/ColaboradorController.cs
+++ SITE/TDSAlmoxarifado/TDSAlmoxarifado/TDSAlmoxarifado/Controllers/ColaboradorController.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TDSAlmoxarifado.Models;
 
 namespace TDSAlmoxarifado.Controllers
 {
     public class ColaboradorController : Controller
     {
+        BDTDSAlmoxarifado bd = new BDTDSAlmoxarifado();
         // GET: Colaborador
         public ActionResult Index()
         {
-            return View();
+            return View(bd.COLABORADOR.ToList());
         }
 
         [HttpGet]
-        public ActionResult Create() 
+        public ActionResult Create(string nome, string cargo, int? codigoarea) 
         {
-            return View();
+            COLABORADOR colaborador = new COLABORADOR();
+            colaborador.COLNOME = nome;
+            colaborador.COLCARGO = cargo;
+            colaborador.AREID = codigoarea;
+
+            try
+            {
+                bd.COLABORADOR.Add(colaborador);
+                bd.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("ErrorDB", "Home");
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
