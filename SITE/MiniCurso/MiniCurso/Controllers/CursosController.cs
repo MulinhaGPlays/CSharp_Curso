8b455cefe32f8ee3263a9f82eb338af1564f947e
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-08-16 15:55:48 -0300

    novo projeto

---
+++ SITE/MiniCurso/MiniCurso/Controllers/CursosController.cs
@@ -0,0 +1,20 @@
+﻿using MiniCurso.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniCurso.Controllers
+{
+    public class CursosController : Controller
+    {
+        masterEntities bd = new masterEntities();
+        // GET: Cursos
+        public ActionResult Index()
+        {
+            ViewBag.qtdCursos = bd.Cursos.ToList().Count();
+            return View(bd.Cursos.ToList());
+        }
+    }
+}
